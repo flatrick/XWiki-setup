9d3e5229615e4c831ada35600180a39582987761
Patrik <flatrick@users.noreply.github.com>
2020-07-15 14:39:35 +0200

    Create upgrade-xwiki.sh

---
+++ non-specific/upgrade-xwiki.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# Get download URL for XWiki war package
+if [ -z ${1} ]; then
+        echo "No url was supplied as an argument for the script"
+        echo "ex: $0 http://url.to.resource/file.name.here.tar.gz"
+        exit #exit script
+elif grep -iqP "(^http[s]?):\/\/.*.war$" <<< ${1}; then
+  XWikiURL=$1
+else
+        echo "No valid url was supplied as an argument for the script"
+        echo "ex: $0 http://nexus.xwiki.org/nexus/content/groups/public/org/xwiki/platform/xwiki-platform-distribution-war/11.10.7/xwiki-platform-distribution-war-11.10.7.war"
+        exit #exit script
+fi
+
+# Set initial values
+NewVersion=`echo ${XWikiURL} | sed -r 's/.*?(xwiki-platform-distribution-war-)([0-9|\.]*)(.war)/\2/'`
+LatestWAR=`echo ${XWikiURL} | sed -r 's/.*\/(.*.war)/\1/'`
+InstallFiles=/opt/install-files
+WebApps=/opt/tomcat/latest/webapps
+
+## Stop tomcat
+service tomcat stop
+
+## Download, move current version and unpack
+cd ${InstallFiles}
+wget ${XWikiURL}
+if ! [ -d ${WebApps}/xwiki ]; then
+ mkdir ${WebApps}/xwiki
+ unzip ${InstallFiles}/${LatestWAR} -d ${WebApps}/xwiki/
+else
+ rm -rf ${InstallFiles}/old_xwiki
+ mv ${WebApps}/xwiki ${InstallFiles}/old_xwiki
+ unzip ${InstallFiles}/${LatestWAR}  -d ${WebApps}/xwiki/
+fi
+
+## Set the correct permissions and ownership
+find ${WebApps}/xwiki -type d -exec chmod 0750 {} \;
+find ${WebApps}/xwiki -type f -exec chmod 0640 {} \;
+chown -RH tomcat: ${WebApps}/xwiki
+
+
+#################################################################
+
+echo "Now it's time to go through the settings found in ${WebApps}/xwiki/xwiki/WEB-INF and ensure it still uses the correct database and other settings.
+You can use the colordiff tool in Linux to compare the old config-file with the new one like this:
+
+# colordiff -yW"\`tput cols\`" ${WebApps}/xwiki/WEB-INF/hibernate.cfg.xml ${InstallFiles}/old_xwiki/WEB-INF/hibernate.cfg.xml | less -R
+# colordiff -yW"\`tput cols\`" ${WebApps}/xwiki/WEB-INF/web.xml ${InstallFiles}/old_xwiki/WEB-INF/web.xml | less -R
+# colordiff -yW"\`tput cols\`" ${WebApps}/xwiki/WEB-INF/xwiki.cfg ${InstallFiles}/old_xwiki/WEB-INF/xwiki.cfg | less -R
+# colordiff -yW"\`tput cols\`" ${WebApps}/xwiki/WEB-INF/xwiki.properties ${InstallFiles}/old_xwiki/WEB-INF/xwiki.properties  | less -R
+
+# Start tomcat again.
+service tomcat start"
